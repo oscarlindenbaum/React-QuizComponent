87a9edcad2c8741f4642be877f80d18e4d0ea169
Oscar <oscarlindenbaum@gmail.com>
2020-07-18 19:37:00 +0100

    starting sections

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import QuizEnd from "./QuizEnd";
 import QuizQuestion from "./QuizQuestion";
+
 let quizData = require('./quiz_data.json')
 
-class Quiz extends Component{
+class Quiz extends Component {
     constructor(props) {
         super(props);
         this.state = {quiz_position: 1};
     }
-    render(){
-        return <div>
-            {isQuizEnd && <QuizEnd/>}
+
+    render() {
+        const isQuizEnd = quizData.quiz_questions.length === this.state.quiz_position - 1;
+        if (isQuizEnd) {
+            return (<QuizEnd/>);
+        }
+        return (<div>
             <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
-        </div>;
+        </div>);
     }
 }
 
--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 class QuizEnd extends Component{
     render(){
-        const isQuizEnd =  quizData.quiz_questions.length==this.state.quiz_position-1;
-        if(isQuizEnd) return (
+        return (
             <div>
                 <p>Thanks for playing!</p>
                 <a href=''>Reset Quiz</a>
