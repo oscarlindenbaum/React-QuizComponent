011868254e70d87b3f6c9ab3b7f7aa7cf5ca23e2
Oscar <oscarlindenbaum@gmail.com>
2020-07-18 18:29:00 +0100

    starting sections

---
+++ src/Quiz.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+let quizData = require('./quiz_data.json')
+
+class Quiz extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {quiz_position: 1};
+    }
+    render(){
+        return <div>
+            <div className="QuizQuestion"></div>
+        </div>;
+    }
+}
+
+export default Quiz;
